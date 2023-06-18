d8f040ad98fcae85cf78294bea95f1a5fa77e6b5
Coderllkey <86723226+Coderllkey@users.noreply.github.com>
2023-06-18 19:34:46 +0630

    jS object creation

---
+++ objects.js
@@ -0,0 +1,16 @@
+// Creating Simple Object 
+
+let Obj = {
+    name: "Nick",
+    age: 23,
+    job: "WEB developer",
+    address : "YGN"
+}
+console.log(Obj.name);
+
+Obj.name = "Nicole";
+console.log(Obj);
+Obj.address = "Mdy";
+console.log(Obj.address);
+
+// Creating Simple Object 
