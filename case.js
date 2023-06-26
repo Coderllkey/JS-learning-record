d4a876cf6f7a656583bb22c67b89dee586a65923
Coderllkey <86723226+Coderllkey@users.noreply.github.com>
2023-06-26 21:44:05 +0630

    Switch statement
    Using switch js

---
+++ case.js
@@ -0,0 +1,12 @@
+let power = 30;
+ 
+switch(power){
+    case 30: console.log("Weak");
+    break;
+    case 50: console.log("Average");
+    break;
+    case 75: console.log("Strong");
+    break;
+    default: console.log("Unidentified");
+    break;
+}
